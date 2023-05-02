d2715c91774af1b49452606a05a76381a4185816
Raphaël Badia <raphael@badia.cc>
2023-05-02 18:49:31 +0200

    more timeout

--- app/api/appdir-edge/route.js
+++ app/api/appdir-edge/route.js
@@ -7,6 +7,9 @@ const data = {
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const id = searchParams.get("userId");
+  await new Promise((resolve) =>
+    setTimeout(resolve, Math.floor(Math.random() * 1000))
+  );
   data.current = id;
   await new Promise((resolve) =>
     setTimeout(resolve, Math.floor(Math.random() * 1000))
--- pages/api/pagedir.js
+++ pages/api/pagedir.js
@@ -3,6 +3,9 @@ const data = {
 };
 
 export default async function handler(req, res) {
+  await new Promise((resolve) =>
+    setTimeout(resolve, Math.floor(Math.random() * 1000))
+  );
   data.current = req.query.userId;
   await new Promise((resolve) =>
     setTimeout(resolve, Math.floor(Math.random() * 1000))
