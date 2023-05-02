36eed32ddf6296f4d3890545cf0c7abe76475e59
Raphaël Badia <raphael@badia.cc>
2023-05-02 18:58:31 +0200

    super slow

--- pages/api/pagedir.js
+++ pages/api/pagedir.js
@@ -3,12 +3,12 @@ const data = {
 };
 
 export default async function handler(req, res) {
-  // await new Promise((resolve) =>
-  //   setTimeout(resolve, Math.floor(Math.random() * 1000))
-  // );
+  await new Promise((resolve) =>
+    setTimeout(resolve, Math.floor(Math.random() * 3000))
+  );
   data.current = req.query.userId;
-  // await new Promise((resolve) =>
-  //   setTimeout(resolve, Math.floor(Math.random() * 1000))
-  // );
+  await new Promise((resolve) =>
+    setTimeout(resolve, Math.floor(Math.random() * 3000))
+  );
   res.json(data);
 }
