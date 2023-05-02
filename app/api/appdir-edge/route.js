3f439b6fc5847a929f0aad94e5cf43f1c481e1ed
Raphaël Badia <raphael@badia.cc>
2023-05-02 17:18:49 +0200

    adasd

---
+++ app/api/appdir-edge/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+const data = {
+  current: 0,
+};
+
+export async function GET(request) {
+  data.current += 1;
+  return NextResponse.json(data);
+}
+
+export const runtime = "experimental-edge";
