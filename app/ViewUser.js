70f1d6285fb032e6fd458ed7540cd7c6129f96f7
Raphaël Badia <raphael@badia.cc>
2023-05-02 18:55:49 +0200

    ad

--- app/ViewUser.js
+++ app/ViewUser.js
@@ -25,11 +25,12 @@ export const ViewUser = ({ user }) => {
   } = useQuery({
     queryKey: ["appdirNode", user.id],
     queryFn: async ({ signal }) => {
-      const resp = await fetch(`/api/appdir?userId=${user.id}`, {
-        signal,
-      });
-      const data = await resp.json();
-      return data;
+      return {};
+      // const resp = await fetch(`/api/appdir?userId=${user.id}`, {
+      //   signal,
+      // });
+      // const data = await resp.json();
+      // return data;
     },
   });
 
@@ -40,11 +41,12 @@ export const ViewUser = ({ user }) => {
   } = useQuery({
     queryKey: ["appdirEdge", user.id],
     queryFn: async ({ signal }) => {
-      const resp = await fetch(`/api/appdir?userId=${user.id}`, {
-        signal,
-      });
-      const data = await resp.json();
-      return data;
+      return {};
+      // const resp = await fetch(`/api/appdir?userId=${user.id}`, {
+      //   signal,
+      // });
+      // const data = await resp.json();
+      // return data;
     },
   });
 
