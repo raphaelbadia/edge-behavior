30079a3d040b6e3377110ace2014fbf5cb499c59
Raphaël Badia <raphael@badia.cc>
2023-05-03 10:36:50 +0200

    wip

--- app/page.js
+++ app/page.js
@@ -7,6 +7,26 @@ const users = new Array(20).fill(undefined).map((_, i) => ({ id: i + 1 }));
 export default function Home() {
   return (
     <main>
+      <h1>Experiment 1 :</h1>
+      <p>
+        The goal is to see if when receiving concurrent requests, the backend
+        gets lost
+      </p>
+      <p>In the API routes:</p>
+      <pre>
+        {`
+      const data = {
+        current: 0,
+      };
+      
+      export default async function handler(req, res) {
+        // wait 0-1000ms
+        data.current = req.query.userId;
+        // wait 0-1000ms
+        res.json(data);
+      }
+      `}
+      </pre>
       <table id="users">
         <tr>
           <th>user id</th>
